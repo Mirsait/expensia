aa1c4082e24d29b585172e932e6ac72bea945aa1
Mirsait <mavcharenko@gmail.com>
2026-01-09 21:47:07 +0300

    Fixed load func and added add-command

--- storage/load.go
+++ storage/load.go
@@ -8,18 +8,21 @@ import (
 	"github.com/Mirsait/expensia/models"
 )
 
-func Load(filename string) ([]models.Expense, error) {
+type Expense = models.Expense
+
+func Load(filename string) ([]Expense, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		if os.IsNotExist(err) {
 			if err := os.WriteFile(filename, []byte("[]"), 0644); err != nil {
 				return nil, fmt.Errorf("create file: %w", err)
 			}
+			return make([]Expense, 0), nil
 		}
 		return nil, fmt.Errorf("read file: %w", err)
 	}
 
-	var result []models.Expense
+	var result []Expense
 	if err = json.Unmarshal(data, &result); err != nil {
 		return nil, fmt.Errorf("parse JSON: %w", err)
 	}
