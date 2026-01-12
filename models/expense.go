20ab3fc798d6931efd576f52ba3e4cbbb8d5d34c
Mirsait <mavcharenko@gmail.com>
2026-01-12 19:20:22 +0300

    Update expense.go

--- models/expense.go
+++ models/expense.go
@@ -7,13 +7,15 @@ type Expense struct {
 	CreatedAt   time.Time `json:"createdAt"`
 	Description string    `json:"description"`
 	Amount      int       `json:"amount"`
+	Category    string    `json:"category"`
 }
 
-func NewExpense(id int, description string, amount int) Expense {
+func NewExpense(id int, description, category string, amount int) Expense {
 	return Expense{
 		Id:          id,
 		Description: description,
 		Amount:      amount,
 		CreatedAt:   time.Now(),
+		Category:    category,
 	}
 }
